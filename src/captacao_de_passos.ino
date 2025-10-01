fe693d2931cd94711d587df94a16daf88598daa7
ste <stefaniep@outlook.com>
2025-10-01 19:07:00 -0300

    correcao algoritmo de captação de passos

--- src/captacao_de_passos.ino
+++ src/captacao_de_passos.ino
@@ -32,22 +32,11 @@ void setup() {
   MPU9250Setting config;
   config.accel_fs_sel = ACCEL_FS_SEL::A2G;  // Faixa do acelerômetro ±2g, aumenta a resolução e é melhor para detectar passos
   config.gyro_fs_sel  = GYRO_FS_SEL::G250DPS; // Faixa do giroscópio ±250°/s, aumenta a precisão para passos
-  config.accel_dlpf_cfg = ACCEL_DLPF_CFG::DLPF_10HZ; // Configura o filtro passa-baixa digital (DLPF) para o acelerômetro, na tentativa de diminuir os ruídos
-  config.gyro_dlpf_cfg = GYRO_DLPF_CFG::DLPF_10HZ; // Configura o filtro passa-baixa digital (DLPF) para o giroscópio
   mpu.setup(0x68, config);  //Inicializa o sensor no endereço 0x68, aplicando as configurações escolhidas
 
-  //Ajustar o número de iterações para o filtro de quaternions (EXPLICAÇÃO: ...)
-  mpu.setFilterIterations(15); // Experimente com 10, 15 ou 20.
 
   // Calibração do acelerômetro + giroscópio (É só deixar o sensor parado)
   mpu.calibrateAccelGyro();
-
-  // // Calibração do magnetômetro pode ser feita se você precisar da orientação (yaw).
-  // Serial.println("Calibrando magnetômetro... Mexa o sensor em forma de oito.");
-  // delay(4000); // Delay para o usuário mover o sensor
-  // mpu.calibrateMag();
-  // Serial.println("Calibração do magnetômetro concluída.");
-  // delay(1000); // Pausa após a calibração
 
   // Correção dos viéses do acelerômetro
   calibratedAccBiasX = mpu.getAccBiasX();
@@ -60,49 +49,48 @@ void setup() {
   mpu.setGyroBias(calibratedGyroBiasX, calibratedGyroBiasY, calibratedGyroBiasZ);
   
   Serial.println("Configuração e Calibração concluída!");
+
+  // Ponteiro para arquivo
+  FILE *fptr;
+
+  // Cria o arquivo
+  fptr = fopen("passos.txt", "w");
+
+  // Fecha o arquivo
+  fclose(fptr); 
 }
 
 void loop() {
 
   // Descomentar se precisar ver os valores de aceleração captados em cada um dos eixos
-  if (mpu.update()) {
-    Serial.print("X, Y, Z: ");
-    Serial.print(mpu.getLinearAccX());
-    Serial.print(", ");
-    Serial.print(mpu.getLinearAccY());
-    Serial.print(", ");
-    Serial.println(mpu.getLinearAccZ());
-
-    Serial.print("Yaw, Pitch, Roll: ");
-    // Serial.print(mpu.getYaw(), 2);
-    Serial.print(mpu.getYaw());
-    Serial.print(", ");
-    // Serial.print(mpu.getPitch(), 2);
-    Serial.print(mpu.getPitch());
-    Serial.print(", ");
-    // Serial.println(mpu.getRoll(), 2);
-    Serial.println(mpu.getRoll());
-
-    delay(50);
-  }
-
   // if (mpu.update()) {
-  //   linearAccX = mpu.getLinearAccX();
-  //   linearAccY = mpu.getLinearAccY();
-  //   linearAccZ = mpu.getLinearAccZ();
+  //   Serial.print(mpu.getLinearAccX());
+  //   Serial.print(", ");
+  //   Serial.print(mpu.getLinearAccY());
+  //   Serial.print(", ");
+  //   Serial.println(mpu.getLinearAccZ());
+  //   delay(50);
+  // }
 
-  //   A = sqrt(pow(linearAccX, 2) + pow(linearAccY, 2) + pow(linearAccZ, 2));
-  //   Serial.println(A);
+  // PROBLEMA: A aceleração muda se mexermos na angulação do sensor
+
+  if (mpu.update()) {
+    linearAccX = mpu.getLinearAccX();
+    linearAccY = mpu.getLinearAccY();
+    linearAccZ = mpu.getLinearAccZ();
+
+    A = sqrt(pow(linearAccX, 2) + pow(linearAccY, 2) + pow(linearAccZ, 2));
+    Serial.println(A);
     
 
-  //   if (A > impactoDoPasso && (millis() - momentoDoPassoAnterior) > tempoEntrePassos) {
-  //     passos++;
-  //     Serial.print("Total de passos: ");
-  //     Serial.println(passos);
-  //     momentoDoPassoAnterior = millis(); //Marca o momento em que o passo foi dado
-  //   }
+    if (A > impactoDoPasso && (millis() - momentoDoPassoAnterior) > tempoEntrePassos) {
+      passos++;
+      Serial.print("Total de passos: ");
+      Serial.println(passos);
+      momentoDoPassoAnterior = millis(); //Marca o momento em que o passo foi dado
+    }
 
-  //   delay(100);
-  // }
+    delay(100);
+  }
   
 }
